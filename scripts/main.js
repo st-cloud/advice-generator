508b0abb0c966e1c3bfbb7565ad1cfebe98cac48
grace.brown <grace.brown@vmlyr.com>
2022-08-04 13:08:11 -0500

    Clean up

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,3 @@
-// $( document ).ready(function() {
-//     console.log( "ready!" );
-
-//     $("btn").click(function(){
-//         $.getJSON("https://api.adviceslip.com/advice", function(data, status){
-//             $(".idnumber").text(data.slip.id);
-//             $(".advice").text(data.slip.advice);
-//         });
-//       });
-// });
-
-// how to insert without erasing existing text?
-
 $( document ).ready(function() {
     console.log( "ready!" );
     // add function names here
