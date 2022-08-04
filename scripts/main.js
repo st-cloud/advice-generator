70a03c55f7c984e12937b688a1783377f6c5d4ed
grace.brown <grace.brown@vmlyr.com>
2022-08-04 10:49:57 -0500

    Restore button function and node modules

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,25 @@
+// $( document ).ready(function() {
+//     console.log( "ready!" );
+
+//     $("btn").click(function(){
+//         $.getJSON("https://api.adviceslip.com/advice", function(data, status){
+//             $(".idnumber").text(data.slip.id);
+//             $(".advice").text(data.slip.advice);
+//         });
+//       });
+// });
+
+// how to insert without erasing existing text?
+
 $( document ).ready(function() {
     console.log( "ready!" );
     // add function names here
 });
 
 // how to insert without erasing existing text?
-$("button").click(function(){
+$(".btn").click(function(){
     $.getJSON("https://api.adviceslip.com/advice", function(data, status){
-        $(".idnumber").text(data.slip.id);
+        $(".count").text(data.slip.id);
         $(".advice").text(data.slip.advice);
     });
-  });
+});
